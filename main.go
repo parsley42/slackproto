fe29fd288db29783ee16058dd1b90a77e0e7d01b
David Parsley <parsley@linuxjedi.org>
2022-05-28 05:02:44 -0400

    Simple raw and variable

--- main.go
+++ main.go
@@ -8,43 +8,25 @@ import (
 	"github.com/slack-go/slack"
 )
 
-// approvalRequest mocks the simple "Approval" template located on block kit builder website
-func generateBlock() slack.MsgOption {
-	// Header Section
-	headerText := slack.NewTextBlockObject("mrkdwn", "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*", false, false)
-	headerSection := slack.NewSectionBlock(headerText, nil, nil)
+func msgBlockRaw(msg string) slack.MsgOption {
+	messageText := slack.NewTextBlockObject("mrkdwn", msg, false, false)
+	messageSection := slack.NewSectionBlock(messageText, nil, nil)
+	return slack.MsgOptionBlocks(messageSection)
+}
 
-	// Fields
-	typeField := slack.NewTextBlockObject("mrkdwn", "*Type:*\nComputer (laptop)", false, false)
-	whenField := slack.NewTextBlockObject("mrkdwn", "*When:*\nSubmitted Aut 10", false, false)
-	lastUpdateField := slack.NewTextBlockObject("mrkdwn", "*Last Update:*\nMar 10, 2015 (3 years, 5 months)", false, false)
-	reasonField := slack.NewTextBlockObject("mrkdwn", "*Reason:*\nAll vowel keys aren't working.", false, false)
-	specsField := slack.NewTextBlockObject("mrkdwn", "*Specs:*\n\"Cheetah Pro 15\" - Fast, really fast\"", false, false)
+func msgBlockPlain(msg string) slack.MsgOption {
+	messageText := slack.NewTextBlockObject("plain_text", msg, false, false)
+	messageSection := slack.NewSectionBlock(messageText, nil, nil)
+	return slack.MsgOptionBlocks(messageSection)
+}
 
-	fieldSlice := make([]*slack.TextBlockObject, 0)
-	fieldSlice = append(fieldSlice, typeField)
-	fieldSlice = append(fieldSlice, whenField)
-	fieldSlice = append(fieldSlice, lastUpdateField)
-	fieldSlice = append(fieldSlice, reasonField)
-	fieldSlice = append(fieldSlice, specsField)
-	inputBlock := slack.NewInputBlock(
-		"url_block",
-		slack.NewTextBlockObject("plain_text", "URL", false, false),
-		slack.NewPlainTextInputBlockElement(slack.NewTextBlockObject("plain_text", "https://www.google.com", false, false), "input_url"),
-	)
-
-	fieldsSection := slack.NewSectionBlock(nil, fieldSlice, nil)
-
-	// Approve and Deny Buttons
-	approveBtnTxt := slack.NewTextBlockObject("plain_text", "Approve", false, false)
-	approveBtn := slack.NewButtonBlockElement("", "click_me_123", approveBtnTxt)
-
-	denyBtnTxt := slack.NewTextBlockObject("plain_text", "Deny", false, false)
-	denyBtn := slack.NewButtonBlockElement("", "click_me_123", denyBtnTxt)
-
-	actionBlock := slack.NewActionBlock("", approveBtn, denyBtn)
-
-	return slack.MsgOptionBlocks(headerSection, fieldsSection, inputBlock, actionBlock)
+func sendBlock(channel string, client slack.Client, block slack.MsgOption) {
+	channelID, timestamp, err := client.PostMessage(channel, slack.MsgOptionText("", false), block)
+	if err != nil {
+		fmt.Printf("Could not send message: %v\n", err)
+	} else {
+		fmt.Printf("Message send successfully to channel %s at %s\n", channelID, timestamp)
+	}
 }
 
 func main() {
@@ -64,10 +46,9 @@ func main() {
 		slack.OptionLog(log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)),
 	)
 
-	message := generateBlock()
-	channelID, timestamp, err := client.PostMessage(channel, slack.MsgOptionText("", false), message)
-	if err != nil {
-		fmt.Printf("Could not send message: %v", err)
-	}
-	fmt.Printf("Message send successfully to channel %s at %s\n", channelID, timestamp)
+	text := "Have a look:\n*<github.com/parsley42|David Parsley - Coder>*"
+	rawMessage := msgBlockRaw(text)
+	sendBlock(channel, *client, rawMessage)
+	plainMessage := msgBlockPlain(text)
+	sendBlock(channel, *client, plainMessage)
 }
